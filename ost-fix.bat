20c37379b53764d5fd04f06e4846165b1e88b48c
Praveen Thumma <8345183+praveenthumma@users.noreply.github.com>
2024-08-29 11:34:50 +0530

    Create ost-fix.bat

---
+++ ost-fix.bat
@@ -0,0 +1,6 @@
+taskkill /F /IM UcMapi.exe /T
+taskkill /F /IM OUTLOOK.EXE /T
+taskkill /F /IM lync.exe /T
+taskkill /F /IM lynchtmlconv.exe /T
+taskkill /F /IM Teams.exe /T
+taskkill /F /IM ms-teams.exe /T
